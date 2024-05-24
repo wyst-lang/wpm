231281858e05912638bb73ab76254343abd3f71b
Leo dev <103524696+kleo-dev@users.noreply.github.com>
2024-05-24 04:28:59 -0700

    Update install-windows.bat

--- install-windows.bat
+++ install-windows.bat
@@ -1,11 +1,16 @@
 @echo off
-
+cd %~dp0
 where go >nul 2>&1
 if %errorlevel% neq 0 (
-    echo "please install go: https://go.dev/doc/install"
+    echo please install go: https://go.dev/doc/install
+    start https://go.dev/doc/install
     exit /b 1
 ) else (
-    xcopy . "%HOMEDRIVE%%HOMEPATH%\" /E /I /Y
-    cd /d "%HOMEDRIVE%%HOMEPATH%\wyst-package-manager"
-    go build -o bin/ src/*.go
+    @echo off
+    xcopy /s /e /i /h /y . "%USERPROFILE%\wyst-package-manager"
+    cd /d "%USERPROFILE%\wyst-package-manager\src"
+    go build -o ..\wpm.exe
+    copy ..\wpm.exe C:\Windows\System32
 )
+
+pause
